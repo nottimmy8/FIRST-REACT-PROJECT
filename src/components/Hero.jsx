b6e65d812999ce22771b6842db4f979de41e961d
oyewole timilehin <oyewoleoluwatimilehin2@gmail.com>
2024-01-18 05:42:06 -1100

    updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,10 +13,7 @@ function Hero() {
             Foods <span>Delivered</span>
           </h1>
         </div>
-        <img
-          src="https://images.pexels.com/photos/1639562/pexels-photo-1639562.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-          alt=""
-        />
+        <img src="" alt="" />
       </div>
     </div>
   );
