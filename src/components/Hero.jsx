3ee613caf505e1b12848240dc93bfe49abacd7d9
oyewole timilehin <oyewoleoluwatimilehin2@gmail.com>
2024-01-18 05:37:55 -1100

    update

---
+++ src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div>
+      <div>
+        {/* Overlay */}
+        <div>
+          <h1>
+            The <span>Best</span>
+          </h1>
+          <h1>
+            Foods <span>Delivered</span>
+          </h1>
+        </div>
+        <img
+          src="https://images.pexels.com/photos/1639562/pexels-photo-1639562.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
